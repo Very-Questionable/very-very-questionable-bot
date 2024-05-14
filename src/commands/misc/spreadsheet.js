7985beb6f3b0ef1379128fd4097c4be241f09aa7
Ian Guo <ianguo8626@gmail.com>
2024-05-14 21:04:13 +1000

    spreadsheet command largely complete

--- src/commands/misc/spreadsheet.js
+++ src/commands/misc/spreadsheet.js
@@ -1,4 +1,7 @@
+import { write } from 'fs';
 import getAllMessages from '../../utils/getAllMessages.js';
+import fs from 'fs/promises';
+import os from 'os';
 
 const speadsheet = {
   name: 'speadsheet',
@@ -6,22 +9,64 @@ const speadsheet = {
   options: [],
   callback: async (client, interaction) => {
     const messages = await getAllMessages(client, interaction.channelId);
-    // console.log(messages.map((msg) => msg.content)); // Print all messages
-    const res = messages.map(msg => {
-      const buffer = msg.content.split(',');
-      const exersise = buffer.splice(0,1);
 
-      for (let i in buffer)
+    const buffer = [];
+    for (const msg of messages) {
+      msg.content.split('\n').forEach((content) => {
+        buffer.push({
+          content: content,
+          timestamp: msg.createdTimestamp,
+        });
+      });
+    }
+    const res = buffer.map((msg) => {
+      const buffer = msg.content.split(',');
+      const exersise = buffer.splice(0, 1);
+      for (let i in buffer) {
+        buffer[i] = buffer[i].trim();
         if (buffer[i] === '-') buffer[i] = buffer[i - 1];
+      }
+      const notes = isNaN(buffer[buffer.length - 1]) ? buffer.pop() : '';
 
       return {
-        timestamp: msg.createdTimestamp,
+        timestamp: new Date(msg.timestamp),
         content: buffer,
-        exersise: exersise[0]
-      }
-    })
-    console.log(res)
+        exersise: exersise[0],
+        notes: notes,
+      };
+    });
+
+    const cleanedData = formatData(res);
+    const dest = await storeSpreadsheet(cleanedData);
+    
+    await interaction.reply({
+      content: "Here is you're spreadsheet :D",
+      files: [dest],
+      ephemeral: true,
+    });
+    
+    console.log(`spreadsheet sent to ${dest}`);
   },
+};
+
+// stores the created spreadsheet in a temporary file
+const storeSpreadsheet = async (data, path) => {
+  const dest = `${!path ? os.tmpdir() : path}/gym-spreadsheet.csv`;
+  await fs.writeFile(dest, data.join(''), 'utf8');
+  console.log(`writen to ${dest}`);
+  return dest;
+};
+
+const formatData = (data) => {
+  data.sort((a, b) => a.timestamp - b.timestamp);
+  const res = data.map(
+    (entry) =>
+      `${entry.exersise},${entry.timestamp.toLocaleDateString('en-GB')},${entry.content.join(',')},${entry.notes}\n`
+  );
+
+  res.unshift('Exersise,Timestamp,Weight 1,Weight 2,Weight 3,Rep 1,Rep 2,Rep 3,Notes\n');
+  res.unshift('\ufeff');
+  return res;
 };
 
 export default speadsheet;
